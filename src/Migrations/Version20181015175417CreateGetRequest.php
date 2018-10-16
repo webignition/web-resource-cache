6e0b3b936f6e92579f91f06b0d4d133c1b1f2fff
Jon Cram <jon@webignition.net>
2018-10-16 13:19:22 +0100

    Fix db migration

--- src/Migrations/Version20181015175417CreateGetRequest.php
+++ src/Migrations/Version20181015175417CreateGetRequest.php
@@ -15,7 +15,7 @@ final class Version20181015175417CreateGetRequest extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE get_request (id INT AUTO_INCREMENT NOT NULL, url LONGTEXT NOT NULL, callback_url LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE get_request (id INT AUTO_INCREMENT NOT NULL, url LONGTEXT NOT NULL, callback_urls LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
