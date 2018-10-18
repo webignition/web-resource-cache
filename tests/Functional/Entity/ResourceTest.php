0b46972e9487a63cef0640fdde4acc71929a962f
Jon Cram <jon@webignition.net>
2018-10-18 09:57:21 +0100

    Create Resource entity

---
+++ tests/Functional/Entity/ResourceTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Tests\Functional\Entity;
+
+use App\Entity\Resource;
+use App\Entity\RetrieveRequest;
+use App\Tests\Functional\AbstractFunctionalTestCase;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ResourceTest extends AbstractFunctionalTestCase
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->entityManager = self::$container->get(EntityManagerInterface::class);
+    }
+
+    /**
+     * @dataProvider createDataProvider
+     *
+     * @param string $url
+     * @param array $headers
+     * @param string $body
+     */
+    public function testCreate(string $url, array $headers, string $body)
+    {
+        $resource = new Resource();
+        $resource->setUrl($url);
+        $resource->setHeaders($headers);
+        $resource->setBody($body);
+
+        $this->assertNull($resource->getId());
+        $this->assertEquals($url, $resource->getUrl());
+        $this->assertEquals($headers, $resource->getHeaders());
+        $this->assertEquals($body, $resource->getBody());
+
+        $this->entityManager->persist($resource);
+        $this->entityManager->flush();
+
+        $this->assertNotNull($resource->getId());
+
+        $id = $resource->getId();
+
+        $this->entityManager->clear();
+
+        /* @var Resource $retrievedResource */
+        $retrievedResource = $this->entityManager->find(Resource::class, $id);
+
+        $this->assertEquals($id, $retrievedResource->getId());
+        $this->assertEquals($url, $retrievedResource->getUrl());
+        $this->assertEquals($headers, $retrievedResource->getHeaders());
+        $this->assertEquals($body, $retrievedResource->getBody());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'empty headers, empty body' => [
+                'url' => 'http://example.com/',
+                'headers' => [],
+                'body' => '',
+            ],
+            'has headers, empty body' => [
+                'url' => 'http://example.com/',
+                'headers' => [
+                    'foo' => 'bar',
+                ],
+                'body' => '',
+            ],
+            'empty headers, has body' => [
+                'url' => 'http://example.com/',
+                'headers' => [],
+                'body' => 'body content',
+            ],
+            'has headers, has body' => [
+                'url' => 'http://example.com/',
+                'headers' => [
+                    'foo' => 'bar',
+                ],
+                'body' => 'body content',
+            ],
+        ];
+    }
+}
