3f762d581c064ff2baf7a8b0a341ca61691a71c8
Jon Cram <jon@webignition.net>
2018-10-19 17:58:06 +0100

    Create ReturnResourceJob

---
+++ src/Resque/Job/ReturnResourceJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Resque\Job;
+
+use App\Command\ReturnResourceCommand;
+use Symfony\Component\Console\Command\Command;
+
+class ReturnResourceJob extends CommandJob
+{
+    const QUEUE_NAME = 'return-resource';
+
+    protected function getQueueName(): string
+    {
+        return self::QUEUE_NAME;
+    }
+
+    protected function getCommand(): Command
+    {
+        /* @var ReturnResourceCommand $command */
+        $command = $this->getContainer()->get(ReturnResourceCommand::class);
+
+        return $command;
+    }
+
+    protected function getCommandArgs(): array
+    {
+        return [
+            'id' => $this->args['id']
+        ];
+    }
+
+    protected function getIdentifier(): string
+    {
+        return $this->args['id'];
+    }
+}
