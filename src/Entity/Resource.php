b2bd2443977d93eee5480258cfbc6bb9a0c513e5
Jon Cram <jon@webignition.net>
2018-10-19 11:18:46 +0100

    Integrate Headers into Entity/Resource

--- src/Entity/Resource.php
+++ src/Entity/Resource.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Model\Headers;
 use App\Model\RequestIdentifier;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -62,30 +63,14 @@ class Resource
         return $this->url;
     }
 
-    public function setHeader(string $key, $value)
+    public function setHeaders(Headers $headers)
     {
-        if (!is_string($value) && !is_int($value)) {
-            return false;
-        }
-
-        $key = strtolower($key);
-
-        $this->headers[$key] = $value;
-        asort($this->headers);
-
-        return true;
+        $this->headers = $headers->toArray();
     }
 
-    public function setHeaders(array $headers)
+    public function getHeaders(): Headers
     {
-        foreach ($headers as $key => $value) {
-            $this->setHeader($key, $value);
-        }
-    }
-
-    public function getHeaders(): array
-    {
-        return $this->headers;
+        return new Headers($this->headers);
     }
 
     public function setBody(string $body)
