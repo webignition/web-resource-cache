7ee86229519a76425f08369a6076bb1a7f7eb3da
Jon Cram <webignition@gmail.com>
2018-10-23 17:06:31 +0100

    SendResponseCommand accepts json-encoded Response (#74)

--- src/Resque/Job/AbstractResourceJob.php
+++ src/Resque/Job/AbstractResourceJob.php
@@ -4,15 +4,5 @@ namespace App\Resque\Job;
 
 abstract class AbstractResourceJob extends AbstractCommandJob
 {
-    protected function getCommandArgs(): array
-    {
-        return [
-            'request-hash' => $this->args['request-hash']
-        ];
-    }
 
-    protected function getIdentifier(): string
-    {
-        return $this->args['request-hash'];
-    }
 }
--- tests/Unit/Resque/SendResponseJobTest.php
+++ tests/Unit/Resque/SendResponseJobTest.php
@@ -2,31 +2,67 @@
 
 namespace App\Tests\Unit\Resque;
 
+use App\Entity\CachedResource;
+use App\Model\Response\AbstractResponse;
+use App\Model\Response\ConnectionFailureResponse;
+use App\Model\Response\HttpFailureResponse;
+use App\Model\Response\SuccessResponse;
+use App\Model\Response\UnknownFailureResponse;
 use App\Resque\Job\SendResponseJob;
 
 class SendResponseJobTest extends \PHPUnit\Framework\TestCase
 {
-    public function testCreate()
+    /**
+     * @dataProvider responseDataProvider
+     *
+     * @param AbstractResponse $response
+     */
+    public function testCreateFoo(AbstractResponse $response)
     {
-        $requestHash = 'example-hash';
-        $returnResourceJob = new SendResponseJob([
-            'request-hash' => $requestHash,
+        $responseJson = json_encode($response);
+
+        $sendResponseJob = new SendResponseJob([
+            'response-json' => json_encode($response),
         ]);
 
-        $this->assertEquals(SendResponseJob::QUEUE_NAME, $returnResourceJob->queue);
-        $this->assertEquals(['request-hash' => $requestHash], $returnResourceJob->args);
+        $this->assertEquals(SendResponseJob::QUEUE_NAME, $sendResponseJob->queue);
+        $this->assertEquals(['response-json' => $responseJson], $sendResponseJob->args);
     }
 
     /**
+     * @dataProvider responseDataProvider
+     *
+     * @param AbstractResponse $response
+     *
      * @throws \Exception
      */
-    public function testRun()
+    public function testRun(AbstractResponse $response)
     {
-        $requestHash = 'example-hash';
-        $retrieveResourceJob = new SendResponseJob([
-            'request-hash' => $requestHash,
+        $sendResponseJob = new SendResponseJob([
+            'response-json' => json_encode($response),
         ]);
 
-        $this->assertTrue($retrieveResourceJob->run([]));
+        $this->assertTrue($sendResponseJob->run([]));
+    }
+
+    /**
+     * @return array
+     */
+    public function responseDataProvider(): array
+    {
+        return [
+            'unknown failure response' => [
+                'response' => new UnknownFailureResponse('request_hash_1'),
+            ],
+            'connection failure response' => [
+                'response' => new ConnectionFailureResponse('request_hash_2', 28),
+            ],
+            'http failure response' => [
+                'response' => new HttpFailureResponse('request_hash_3', 404),
+            ],
+            'success response' => [
+                'response' => new SuccessResponse('request_hash_4', new CachedResource()),
+            ],
+        ];
     }
 }
