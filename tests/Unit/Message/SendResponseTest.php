299cadf3a43342640dec460346d63d4fef8ded77
Jon Cram <webignition@gmail.com>
2018-10-29 14:22:08 +0000

    Add Response to SendResponse message (#181)

--- tests/Unit/Message/SendResponseTest.php
+++ tests/Unit/Message/SendResponseTest.php
@@ -3,15 +3,16 @@
 namespace App\Tests\Unit\Message;
 
 use App\Message\SendResponse;
+use App\Model\Response\ResponseInterface;
 
 class SendResponseTest extends \PHPUnit\Framework\TestCase
 {
     public function testCreate()
     {
-        $requestHash = 'request_hash';
+        $response = \Mockery::mock(ResponseInterface::class);
 
-        $retrieveRequest = new SendResponse($requestHash);
+        $message = new SendResponse($response);
 
-        $this->assertEquals($requestHash, $retrieveRequest->getRequestHash());
+        $this->assertEquals($response, $message->getResponse());
     }
 }
