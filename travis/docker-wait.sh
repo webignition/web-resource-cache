f209fd18816eab315bfbbe73a874eacf21603364
Jon Cram <jon@webignition.net>
2018-11-02 17:07:48 +0000

    Add phpmyadmin service

--- travis/docker-wait.sh
+++ travis/docker-wait.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-while ! mysql -uroot -p$WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD --host web-resource-cache-mysql-host --port 33066 -e 'status'; do
+# Wait for mysql to be accepting connections
+echo "Waiting for mysql to accept connections ..."
+while ! mysql -uroot -p$WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD --host web-resource-cache-mysql-host --port 33066 -e 'SELECT 1'; do
+    sleep 1
+done
+
+echo "Waiting for phpmyadmin to be up ..."
+while ! curl -I http://localhost:8080; do
     sleep 1
 done
