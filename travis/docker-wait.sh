9109b1e19a8cc32015cc48e88b4ed1657bd04970
Jon Cram <jon@webignition.net>
2018-11-02 16:53:48 +0000

    Neater way of verifying status of mysql inside docker container

--- travis/docker-wait.sh
+++ travis/docker-wait.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-while ! mysqladmin -uroot -p$WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD --host web-resource-cache-mysql-host --port 33066 --count=1 --sleep=1 ping --silent; do
+while ! mysql -uroot -p$WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD --host web-resource-cache-mysql-host --port 33066 -e 'status'; do
     sleep 1
 done
