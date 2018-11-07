386b75a8436217724b7158aecd11c3e6e3a90cbc
Jon Cram <jon@webignition.net>
2018-11-07 16:38:07 +0000

    Try running migrations differently

--- docker/init.sh
+++ docker/init.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-"${WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD:=root}"
-
-docker-compose up -d
+docker-compose up -d --build
 
 echo "Waiting for mysql to accept connections ..."
 while ! mysql -uroot -p${WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD} --host web-resource-cache-mysql-host --port 33066 -e 'SELECT 1'; do
     sleep 1
 done
 
-docker exec web-resource-cache-app-web ./bin/console doctrine:migrations:migrate --no-interaction
+docker-compose exec -T app-web ./bin/console doctrine:migrations:migrate --no-interaction
