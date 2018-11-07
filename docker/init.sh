935a2d8641a42a12e3d297a1e30b471dd9f391c5
Jon Cram <jon@webignition.net>
2018-11-07 16:48:42 +0000

    Examine failed container logs

--- docker/init.sh
+++ docker/init.sh
@@ -8,4 +8,6 @@ while ! mysql -uroot -p${WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD} --host web-reso
 done
 
 docker-compose ps
+docker logs web-resource-cache-app-web
+docker logs web-resource-cache-app-cli
 docker-compose exec -T app-web ./bin/console doctrine:migrations:migrate --no-interaction
