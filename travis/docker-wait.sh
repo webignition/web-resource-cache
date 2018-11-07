b72c25df39e6b60c308070da1a025eb8549f3585
Jon Cram <webignition@gmail.com>
2018-11-07 15:25:45 +0000

    Use `dockerize -wait` to ensure services are up (#258)

--- travis/docker-wait.sh
+++ travis/docker-wait.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env bash
 
+"${WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD:=}"
+
 # Wait for mysql to be accepting connections
 echo "Waiting for mysql to accept connections ..."
-while ! mysql -uroot -p$WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD --host web-resource-cache-mysql-host --port 33066 -e 'SELECT 1'; do
-    sleep 1
-done
-
-echo "Waiting for phpmyadmin to be up ..."
-while ! curl -I http://localhost:8080; do
-    sleep 1
-done
-
-echo "Waiting for rabbitmq management interface to be up ..."
-while ! curl -I http://localhost:15672; do
-    sleep 1
-done
-
-echo "Waiting for nginx to be up ..."
-while ! curl -d "url=http://example.com/&callback=http://callback.example.com" -X POST http://localhost:8001/; do
+while ! mysql -uroot -p${WEB_RESOURCE_CACHE_MYSQL_ROOT_PASSWORD} --host web-resource-cache-mysql-host --port 33066 -e 'SELECT 1'; do
     sleep 1
 done
