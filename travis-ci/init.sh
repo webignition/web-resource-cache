1f880c06a208b9e9d5ff4a0373f199f585861f7c
Jon Cram <jon@webignition.net>
2018-11-21 13:27:17 +0000

    Verify running containers

--- travis-ci/init.sh
+++ travis-ci/init.sh
@@ -2,7 +2,8 @@
 
 cd docker
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.phpmyadmin.yml up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml ps
 
 echo "Waiting for mysql to accept connections ..."
 while ! mysql -uroot -p${ASYNC_HTTP_RETRIEVER_MYSQL_ROOT_PASSWORD} --host mysql --port 33066 -e 'SELECT 1'; do
