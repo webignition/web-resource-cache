3d3d3bb99fcef033408b6c79a5eaa737554f1138
Jon Cram <jon@webignition.net>
2018-11-21 13:37:54 +0000

    Restart containers after installing deps and running migrations

--- travis-ci/init.sh
+++ travis-ci/init.sh
@@ -3,7 +3,6 @@
 cd docker
 
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml ps
 
 echo "Waiting for mysql to accept connections ..."
 while ! mysql -uroot -p${ASYNC_HTTP_RETRIEVER_MYSQL_ROOT_PASSWORD} --host mysql --port 33066 -e 'SELECT 1'; do
@@ -12,5 +11,10 @@ done
 
 docker-compose exec -T app-web composer install
 docker-compose exec -T app-web ./bin/console doctrine:migrations:migrate --no-interaction
+
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml stop
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml ps
+docker-compose logs app-cli
 
 cd ..
