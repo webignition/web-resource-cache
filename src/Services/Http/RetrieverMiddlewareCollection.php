c9b5fa5d41d97202d91b28e7771cd881d022d0e3
Jon Cram <jon@webignition.net>
2018-12-18 19:22:41 +0000

    Add auth middleware to retriever client

--- src/Services/Http/RetrieverMiddlewareCollection.php
+++ src/Services/Http/RetrieverMiddlewareCollection.php
@@ -5,20 +5,24 @@ namespace App\Services\Http;
 use App\Services\ArrayCollection;
 use GuzzleHttp\Middleware;
 use Kevinrob\GuzzleCache\CacheMiddleware;
+use webignition\Guzzle\Middleware\HttpAuthentication\HttpAuthenticationMiddleware;
 use webignition\HttpHistoryContainer\Container as HttpHistoryContainer;
 
 class RetrieverMiddlewareCollection extends ArrayCollection
 {
+    const MIDDLEWARE_HTTP_AUTHENTICATION_KEY = 'http-authentication';
     const MIDDLEWARE_CACHE_KEY = 'cache';
     const MIDDLEWARE_RETRY_KEY = 'retry';
     const MIDDLEWARE_HISTORY_KEY = 'history';
 
     public function __construct(
+        HttpAuthenticationMiddleware $httpAuthenticationMiddleware,
         HttpHistoryContainer $historyContainer,
         RetryMiddlewareFactory $retryMiddlewareFactory,
         CacheMiddleware $cacheMiddleware
     ) {
         parent::__construct([
+            self::MIDDLEWARE_HTTP_AUTHENTICATION_KEY => $httpAuthenticationMiddleware,
             self::MIDDLEWARE_CACHE_KEY => $cacheMiddleware,
             self::MIDDLEWARE_RETRY_KEY => $retryMiddlewareFactory->create(),
             self::MIDDLEWARE_HISTORY_KEY => Middleware::history($historyContainer),
